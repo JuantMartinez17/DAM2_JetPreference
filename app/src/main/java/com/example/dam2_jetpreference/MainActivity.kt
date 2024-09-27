2319dfb0ad4153b8753734b7d2f6bca6911ecebc
JuantMartinez17 <juantomasm0@gmail.com>
2024-09-27 20:33:35 -0300

    Alert Dialog to confirm task removal

--- app/src/main/java/com/example/dam2_jetpreference/MainActivity.kt
+++ app/src/main/java/com/example/dam2_jetpreference/MainActivity.kt
@@ -1,16 +1,33 @@
 package com.example.dam2_jetpreference
 
 import android.os.Bundle
+import androidx.activity.viewModels
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import com.example.dam2_jetpreference.adapter.TaskAdapter
 import com.example.dam2_jetpreference.databinding.ActivityMainBinding
+import com.example.dam2_jetpreference.viewmodel.TaskViewModel
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
+    private val viewModel: TaskViewModel by viewModels()
+    private lateinit var adapter: TaskAdapter
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
 
+    }
+
+    private fun deleteConfirmation(task: String){
+        AlertDialog.Builder(this)
+            .setTitle("Confirar borrado de tarea")
+            .setMessage("Esta seguro de eliminar la tarea?")
+            .setPositiveButton("Eliminar"){_,_ ->
+                viewModel.removeTask(task)
+            }
+            .setNegativeButton("Cancelar", null)
+            .show()
     }
 }
