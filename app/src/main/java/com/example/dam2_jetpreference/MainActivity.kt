7be5589a620fdcca9ceb57b8a9667face14d4e2e
JuantMartinez17 <juantomasm0@gmail.com>
2024-09-27 20:45:22 -0300

    main activity finished

--- app/src/main/java/com/example/dam2_jetpreference/MainActivity.kt
+++ app/src/main/java/com/example/dam2_jetpreference/MainActivity.kt
@@ -1,9 +1,11 @@
 package com.example.dam2_jetpreference
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.dam2_jetpreference.adapter.TaskAdapter
 import com.example.dam2_jetpreference.databinding.ActivityMainBinding
 import com.example.dam2_jetpreference.viewmodel.TaskViewModel
@@ -17,7 +19,24 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        adapter = TaskAdapter { task -> deleteConfirmation(task) }
+        binding.rvTaskList.adapter = adapter
+        binding.rvTaskList.layoutManager = LinearLayoutManager(this)
 
+        viewModel.tasks.observe(this) { tasks ->
+            adapter.updateTask(tasks)
+        }
+
+        binding.btnAddTask.setOnClickListener {
+            val task = binding.etTaskInput.text.toString()
+            if (!task.isNullOrBlank()) {
+                viewModel.addTask(task)
+                binding.etTaskInput.text.clear()
+            }else{
+                Toast.makeText(this, "Ingrese una tarea para ser agregada", Toast.LENGTH_SHORT)
+                    .show()
+            }
+        }
     }
 
     private fun deleteConfirmation(task: String){
